76da9f9e96bcaf72fd1c0c635d6ac6313f6e36ca
Sawyer X <xsawyerx@cpan.org>
2016-04-23 13:44:35 +0200

    Refactor CLI logger into Pakket::Log:
    Instead of each CLI app having to create a logger from scratch,
    we now refactor that consideration into Pakket::Log.
    
    However, Pakket::Log cannot set the logger. That won't work. Ugh.

--- lib/Pakket/Log.pm
+++ lib/Pakket/Log.pm
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 use parent 'Log::Contextual';
 use Log::Dispatch;
+use Path::Tiny qw< path >;
 
 sub arg_default_logger {
     $_[1] || Log::Dispatch->new(
@@ -16,6 +17,36 @@ sub arg_default_logger {
             ],
         ],
     );
+}
+
+sub cli_logger {
+    my ( $class, $verbose ) = @_;
+    $verbose ||= 0;
+
+    my $screen_level =
+        $verbose >= 3 ? 'debug'  : # log 2
+        $verbose == 2 ? 'info'   : # log 1
+        $verbose == 1 ? 'notice' : # log 0
+                        'warning';
+
+    my $logger = Log::Dispatch->new(
+        outputs => [
+            [
+                'File',
+                min_level => 'debug',
+                filename  => path( Path::Tiny->cwd, 'build.log' )->stringify,
+                newline   => 1,
+            ],
+
+            [
+                'Screen',
+                min_level => $screen_level,
+                newline   => 1,
+            ],
+        ],
+    );
+
+    return $logger;
 }
 
 sub arg_levels {
