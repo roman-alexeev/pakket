bc0849ca9acf02fbb972d70c91dd6232d71b34e3
Sawyer X <xsawyerx@cpan.org>
2016-06-10 12:32:41 +0200

    Support Module::Build installers

--- lib/Pakket/Builder.pm
+++ lib/Pakket/Builder.pm
@@ -411,16 +411,31 @@ sub build_perl_package {
     };
 
     my $original_dir = Path::Tiny->cwd;
+    my $install_base = $prefix->absolute;
 
-    $self->run_command(
-        $build_dir,
-        [ "$^X", 'Makefile.PL', "INSTALL_BASE=$prefix" ],
-        $opts,
-    );
+    if ( $build_dir->child('Makefile.PL')->exists ) {
+        $self->run_command(
+            $build_dir,
+            [ "$^X", 'Makefile.PL', "INSTALL_BASE=$install_base" ],
+            $opts,
+        );
 
-    $self->run_command( $build_dir, ['make'], $opts );
+        $self->run_command( $build_dir, ['make'], $opts );
+        $self->run_command( $build_dir, ['make', 'install'], $opts );
+    } elsif ( $build_dir->child('Build.PL')->exists ) {
+        $self->run_command(
+            $build_dir,
+            [ "$^X", 'Build.PL', '--install_base', $install_base ],
+            $opts,
+        );
 
-    $self->run_command( $build_dir, ['make', 'install'], $opts );
+        $self->run_command( $build_dir, ['./Build'], $opts );
+        $self->run_command( $build_dir, ['./Build', 'install'], $opts );
+    } else {
+        die "Could not find an installer (Makefile.PL/Build.PL)\n";
+    }
+
+    chdir $original_dir;
 
     log_info { "Done preparing $package" };
 }
