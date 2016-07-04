d3a80eae0bfde5b78de984a01bf15344b5cf9e1d
Alexey Surikov <alexey.surikov@booking.com>
2016-07-05 01:36:25 +0200

    teach fetch_source.pl to download specific versions
    since version range semantics are currently undefined in TOML files (to my
    knowledge) and there's no code for that in Pakket, we can't assume that
    "version = 1.42" means "at least 1.42", like it does in cpanfiles
    instead we treat it literally, which means we need to be able to download
    different versions

--- tools/fetch_source.pl
+++ tools/fetch_source.pl
@@ -1,20 +1,59 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
 
-use File::Basename 'basename';
 use MetaCPAN::Client;
+use Getopt::Long;
+use Path::Tiny;
+use TOML;
 
-$|++;
+Getopt::Long::GetOptions(
+    "source-dir=s" => \my $source_dir,
+    "config-dir=s" => \my $config_dir,
+);
+-d $config_dir or die "Invalid config dir";
+-d $source_dir or die "Invalid source dir";
 
-# find metacpan-example/configs/perl/ -type d -exec basename {} \; | sort
-my @dists = @ARGV or die "$0 DISTRIBUTIONS\n";
+my %seen;
+my $iter = path($config_dir)->iterator( { recurse => 1 } );
+while ( my $next = $iter->() ) {
+    if ( not $next->is_file or not $next =~ /\.toml$/ ) {
+        next;
+    }
 
-my $mcpan = MetaCPAN::Client->new;
-foreach my $dist_name (@dists) {
-    my $release = $mcpan->release($dist_name);
-    my $url     = $release->download_url;
-    -f basename($url) and next;
-    print "-> ", $release->name, "\n";
-    system("wget $url");
+    my $module = TOML::from_toml( $next->slurp_utf8 );
+
+    $seen{"$module->{Package}{name}-$module->{Package}{version}"} = undef;
+
+    for my $prereqs ( values( %{ $module->{Prereqs}{perl} } ) ) {
+        for my $dist ( keys(%$prereqs) ) {
+            if ( $dist eq "perl" ) {
+                next;
+            }
+            $seen{"$dist-$prereqs->{$dist}{version}"} = undef;
+        }
+    }
 }
+
+my @to_fetch = sort( keys(%seen) );
+my $mcpan    = MetaCPAN::Client->new;
+
+open( my $pipe, "| wget --directory-prefix='$source_dir' --input-file=-" );
+
+for my $dist (@to_fetch) {
+    my $res = $mcpan->release(
+        {
+            all => [ { name => $dist } ],
+        }
+    );
+    if ( $res->total == 0 ) {
+        warn "Couldn't find $dist on MetaCPAN";
+        next;
+    }
+
+    while ( my $release = $res->next ) {
+        print $pipe $release->download_url, "\n";
+    }
+}
+
+close($pipe);
