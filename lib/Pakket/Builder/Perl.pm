1d88988f9160aaa75ca46cb730b8e0cf955e9316
Sawyer X <xsawyerx@cpan.org>
2016-09-05 16:52:25 +0200

    Finally break apart builders to multiple classes:
    This has bugged me for a while. All the builders are now proper
    classes, based on category.
    
    There is a role that allows all builders to make sure they can
    run commands and generate the proper environment variables.
    
    Perl contains the code for both ExtUtils::MakeMaker and
    Module::Build, while System delegates to a Makefile based one,
    so it could also allow CMake and SCons when we encounter them in
    the future.
    
    What bugs me is that we fetch configure flags (which might or
    might not exist) and then send it everywhere. Instead I would
    prefer to have a general hash of metadata in which the configure
    flags would be one possible field.
    
    I'm still not sure how to introduce additional categories without
    loading them in the Builder.pm object and adding them to the
    "builders" attribute. Two possibilities exist:
    
    1. Make this automatically load with Module::Pluggable, but that
       adds a problem when newer versions possibly remove outdated
       builders. They might remain from older versions and `cpan(m?)`
       doesn't remove before upgrading.
    
    2. Add a configuration option to include additional ones, which
       makes more sense.
    
    This closes GH #12.

---
+++ lib/Pakket/Builder/Perl.pm
@@ -0,0 +1,99 @@
+package Pakket::Builder::Perl;
+# ABSTRACT: Build Perl Pakket packages
+
+use Moose;
+use English '-no_match_vars';
+use Log::Any   qw< $log >;
+use Path::Tiny qw< path >;
+use Pakket::Log;
+
+with qw<Pakket::Role::Builder>;
+
+sub build_package {
+    my ( $self, $package, $build_dir, $prefix ) = @_;
+
+    $log->info("Building Perl module: $package");
+
+    my @perl5lib = ( path( $prefix, qw<lib perl5> )->absolute->stringify );
+
+    my $opts = {
+        'env' => {
+            'PERL5LIB'                  => join( ':', @perl5lib ),
+            'PERL_LOCAL_LIB_ROOT'       => '',
+            'PERL5_CPAN_IS_RUNNING'     => 1,
+            'PERL5_CPANM_IS_RUNNING'    => 1,
+            'PERL5_CPANPLUS_IS_RUNNING' => 1,
+            'PERL_MM_USE_DEFAULT'       => 1,
+            'PERL_MB_OPT'               => '',
+            'PERL_MM_OPT'               => '',
+
+            $self->generate_env_vars( 'prefix' => $prefix ),
+        },
+    };
+
+    my $original_dir = Path::Tiny->cwd;
+    my $install_base = $prefix->absolute;
+
+    # taken from cpanminus
+    my %should_use_mm = map +( "perl/$_" => 1 ),
+        qw( version ExtUtils-ParseXS ExtUtils-Install ExtUtils-Manifest );
+
+    my @seq;
+    if ( $build_dir->child('Build.PL')->exists
+        && !exists $should_use_mm{$package} )
+    {
+        @seq = (
+
+            # configure
+            [
+                $build_dir,
+                [ 'perl', 'Build.PL', '--install_base', $install_base ],
+                $opts,
+            ],
+
+            # build
+            [ $build_dir, ['./Build'], $opts ],
+
+            # install
+            [ $build_dir, [ './Build', 'install' ], $opts ],
+        );
+    } elsif ( $build_dir->child('Makefile.PL')->exists ) {
+        @seq = (
+
+            # configure
+            [
+                $build_dir,
+                [ 'perl', 'Makefile.PL', "INSTALL_BASE=$install_base" ],
+                $opts,
+            ],
+
+            # build
+            [ $build_dir, ['make'], $opts ],
+
+            # install
+            [ $build_dir, [ 'make', 'install' ], $opts ],
+        );
+    } else {
+        die "Could not find an installer (Makefile.PL/Build.PL)\n";
+    }
+
+    my $success = $self->run_command_sequence(@seq);
+
+    if ( !$success ) {
+        $log->critical("Failed to build $package");
+        exit 1;
+    }
+
+    $log->info("Done preparing $package");
+
+    return;
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=pod
---
+++ lib/Pakket/Role/Builder.pm
@@ -0,0 +1,48 @@
+package Pakket::Role::Builder;
+
+# ABSTRACT: A role for all builders
+
+use Moose::Role;
+
+with qw<Pakket::Role::RunCommand>;
+
+requires qw<build_package>;
+
+sub generate_env_vars {
+    my ( $self, $prefix ) = @_;
+    my $lib_path = $self->generate_lib_path($prefix);
+    my $bin_path = $self->generate_bin_path($prefix);
+
+    return (
+        'CPATH'           => $prefix->child('include'),
+        'LD_LIBRARY_PATH' => $lib_path,
+        'LIBRARY_PATH'    => $lib_path,
+        'PATH'            => $bin_path,
+    );
+}
+
+sub generate_lib_path {
+    my ( $self, $prefix ) = @_;
+
+    my $lib_path = $prefix->absolute->child('lib')->stringify;
+    if ( defined( my $env_library_path = $ENV{'LD_LIBRARY_PATH'} ) ) {
+        $lib_path .= ":$env_library_path";
+    }
+
+    return $lib_path;
+}
+
+sub generate_bin_path {
+    my ( $self, $prefix ) = @_;
+
+    my $bin_path = $prefix->child('bin')->absolute->stringify;
+    if ( defined( my $env_bin_path = $ENV{'PATH'} ) ) {
+        $bin_path .= ":$env_bin_path";
+    }
+
+    return $bin_path;
+}
+
+no Moose::Role;
+
+1;
