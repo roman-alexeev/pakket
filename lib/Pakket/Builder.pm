021fd1c6d114bbf3fc1be1ca09cacd083c1cd951
Sawyer X <xsawyerx@cpan.org>
2016-10-13 17:39:32 +0200

    Maybe this time...

--- lib/Pakket/Builder.pm
+++ lib/Pakket/Builder.pm
@@ -174,7 +174,7 @@ sub get_latest_satisfying_version {
         [ keys %{ $self->index->{$category}{$package_name}{versions} } ] );
 
     if ( !$chosen ) {
-        $log->critical(
+        $log->criticalf(
                 "Couldn't find maximum satisfying version for %s in index\n",
                 "$category/$package_name",
         );
