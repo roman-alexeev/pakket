b469c81b8208bf8b2c9e9eee5f695640956d5b6e
Sawyer X <xsawyerx@cpan.org>
2016-10-19 14:11:25 +0200

    System packages are now Native packages

--- lib/Pakket/Builder/Native.pm
+++ lib/Pakket/Builder/Native.pm
@@ -1,11 +1,11 @@
-package Pakket::Builder::System;
-# ABSTRACT: Build System Pakket packages
+package Pakket::Builder::Native;
+# ABSTRACT: Build Native Pakket packages
 
 use Moose;
 use MooseX::StrictConstructor;
 use Log::Any qw< $log >;
 use Pakket::Log;
-use Pakket::Builder::System::Makefile;
+use Pakket::Builder::Native::Makefile;
 
 with qw<Pakket::Role::Builder>;
 
@@ -15,10 +15,10 @@ sub build_package {
     if (   $build_dir->child('configure')->exists
         || $build_dir->child('config')->exists )
     {
-        my $builder = Pakket::Builder::System::Makefile->new();
+        my $builder = Pakket::Builder::Native::Makefile->new();
         $builder->build_package( $package, $build_dir, $prefix, $flags );
     } else {
-        $log->critical("I cannot build this system package. No 'configure'.");
+        $log->critical("I cannot build this native package. No 'configure'.");
         exit 1;
     }
 
