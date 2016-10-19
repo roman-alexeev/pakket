ed269b51f8e64ccd5edc5d3c82c61ddef7e43820
Sawyer X <xsawyerx@cpan.org>
2016-10-19 22:08:25 +0200

    Introduce the Package object:
    This is still a work in progress but here's the first draft. It's
    meant to introduce a part of it without breaking anything.
    
    First we have the Pakket::Package class which provides the
    basic attributes for name, category, and version.
    
    Then we add the requirements (config, test, runtime, and the
    general "prereqs" which contain everything).
    
    We also introduce the versioning as part of the Package class, and
    it determines by itself what the appropriate versioning scheme is.
    
    This removes the 'versioning.toml' file. That approach had two
    flaws: 1. It requires file-based configuration, which won't work
    once we moved outside directory-based structure. 2. It means that
    a package does not contain the knowledge of how to do diffs for it.
    
    For the majority, the rest of the code still uses about the same
    code, but passes the package object between methods and uses that
    instead. Since the versioning scheme is now part of the Package
    class, that had been cleaned up.

--- lib/Pakket/Version/Requirements.pm
+++ lib/Pakket/Version/Requirements.pm
@@ -9,11 +9,6 @@ my @valid_schemas = qw<
     nodejs
 >;
 
-my %category2schema = (
-    perl   => 'perl',
-    nodejs => 'nodejs',
-);
-
 has schema_name => (
     is       => 'ro',
     isa      => enum(\@valid_schemas),
@@ -35,18 +30,6 @@ sub _build_schema {
     my $schema_name = ucfirst $self->schema_name;
 
     return use_module("Pakket::Version::Schema::${schema_name}")->new;
-}
-
-sub new_from_schema {
-    my ( $class, $schema ) = @_;
-
-    return $class->new( { schema_name => $schema } );
-}
-
-sub new_from_category {
-    my ( $class, $category ) = @_;
-
-    return $class->new( { schema_name => $category2schema{$category} } );
 }
 
 sub pick_maximum_satisfying_version {
