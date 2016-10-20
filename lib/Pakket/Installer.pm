a1e7ff317fb90df2bd1437e2dd883461b5fb9a63
Sawyer X <xsawyerx@cpan.org>
2016-10-20 12:39:00 +0200

    Fix installer to use Package objects:
    We have the same problem here: converting strings to packages and
    handling prereqs as packages.

--- lib/Pakket/Installer.pm
+++ lib/Pakket/Installer.pm
@@ -92,6 +92,20 @@ sub install {
                 'version'  => $content->{$category}{$_}{'latest'},
             ) for keys %{ $content->{$category} };
         }
+    } else {
+        my @clean_packages;
+        foreach my $package_str (@packages) {
+            my ( $pkg_cat, $pkg_name, $pkg_ver ) =
+                split /\//xms, $package_str;
+
+            push @clean_packages, Pakket::Package->new(
+                'category' => $pkg_cat,
+                'name'     => $pkg_name,
+                'version'  => $pkg_ver,
+            );
+        }
+
+        @packages = @clean_packages;
     }
 
     if ( !@packages ) {
@@ -174,22 +188,23 @@ sub install {
 sub install_package {
     my ( $self, $package, $dir, $installed ) = @_;
 
-    $log->debug("About to install $package (into $dir)");
+    my $pkg_cat  = $package->category;
+    my $pkg_name = $package->name;
+    my $pkg_ver  = $package->version;
 
-    if ( $installed->{$package}++ ) {
-        $log->debug("$package already installed");
+    $log->debug("About to install $pkg_name (into $dir)");
+
+    if ( $installed->{$pkg_name}++ ) {
+        $log->debug("$pkg_name already installed");
         return;
     }
 
-    my ( $pkg_cat, $pkg_name, $pkg_ver ) = split /\//xms, $package;
-
-    my $req   = Pakket::Version::Requirements->new_from_category($pkg_cat);
+    my $req   = $package->versioning_requirements;
     my $index = $self->index;
 
     my $version = $req->pick_maximum_satisfying_version(
         [ keys %{ $index->{$pkg_cat}{$pkg_name}{'versions'} } ],
     );
-
 
     my $parcel_file = $self->parcel_dir->child(
         $pkg_cat,
@@ -255,7 +270,13 @@ sub install_package {
             #       We should just ask the repo for it
             #       It should maintain metadata for this
             #       and API to retrieve it
-            my $next_pkg = "$prereq_category/$prereq_name/$prereq_version";
+            # FIXME This shouldn't be a package but a prereq object
+            my $next_pkg = Pakket::Package->new(
+                'category' => $prereq_category,
+                'name'     => $prereq_name,
+                'version'  => $prereq_version,
+            );
+
             $self->install_package( $next_pkg, $dir, $installed );
         }
     }
