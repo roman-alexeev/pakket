28edce9e0e507c4c60ca21d96b3b401cb0156d94
Sawyer X <xsawyerx@cpan.org>
2016-12-16 20:41:12 +0100

    add strictures, quote

--- lib/Pakket/Runner.pm
+++ lib/Pakket/Runner.pm
@@ -1,13 +1,16 @@
 package Pakket::Runner;
 
+use strict;
+use warnings;
+
 sub run {
     my ($self, %args) = @_;
 
     my $active_path = $args{'active_path'};
 
-    local $ENV{PATH}            = "$active_path/bin:$ENV{PATH}";
-    local $ENV{PERL5LIB}        = "$active_path/lib/perl5";
-    local $ENV{LD_LIBRARY_PATH} = "$active_path/lib:$ENV{LD_LIBRARY_PATH}";
+    local $ENV{'PATH'}            = "$active_path/bin:$ENV{PATH}";
+    local $ENV{'PERL5LIB'}        = "$active_path/lib/perl5";
+    local $ENV{'LD_LIBRARY_PATH'} = "$active_path/lib:$ENV{LD_LIBRARY_PATH}";
 
     system @args;
 }
