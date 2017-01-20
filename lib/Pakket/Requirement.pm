198a57bc4e0b0d4e18eef35e9ed417f08a54f189
Sawyer X <xsawyerx@cpan.org>
2017-01-20 13:47:34 +0100

    Make version in Pakket::Requirement handle LATEST_VERSION:
    We can now feed 'LATEST_VERSION' as the version of a package.
    
    As a bonus, you can now create a requirement from a requirement
    string.

--- lib/Pakket/Requirement.pm
+++ lib/Pakket/Requirement.pm
@@ -4,13 +4,40 @@ package Pakket::Requirement;
 use Moose;
 use MooseX::StrictConstructor;
 
+use Log::Any          qw< $log >;
+use Pakket::Constants qw< PAKKET_PACKAGE_SPEC >;
+use Pakket::Types;
+
 with qw< Pakket::Role::PrintableNames >;
 
-has [qw< category name version >] => (
+has [qw< category name >] => (
     'is'       => 'ro',
     'isa'      => 'Str',
     'required' => 1,
 );
+
+has 'version' => (
+    'is'       => 'ro',
+    'isa'      => 'PakketVersion',
+    'required' => 1,
+    'coerce'   => 1,
+);
+
+sub new_from_string {
+    my ( $class, $req_str ) = @_;
+
+    if ( $req_str !~ PAKKET_PACKAGE_SPEC() ) {
+        $log->critical("Cannot parse $req_str");
+        exit 1;
+    } else {
+        # This shuts up Perl::Critic
+        return $class->new(
+            'category' => $1,
+            'name'     => $2,
+            'version'  => $3,
+        );
+    }
+}
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
