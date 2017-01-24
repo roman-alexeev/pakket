c75a6260513f75af3fbd85e7fb9d8d1a5983013e
Sawyer X <xsawyerx@cpan.org>
2017-01-24 18:23:06 +0100

    Set up Plack::Runner to serve webapp

--- lib/Pakket/Server.pm
+++ lib/Pakket/Server.pm
@@ -6,6 +6,8 @@ use MooseX::StrictConstructor;
 
 use Log::Any          qw< $log >;
 use Types::Path::Tiny qw< Path >;
+use Plack::Runner;
+
 use Pakket::Server::App;
 
 has 'data_dir' => (
@@ -15,11 +17,25 @@ has 'data_dir' => (
     'required' => 1,
 );
 
+has 'port' => (
+    'is'        => 'ro',
+    'isa'       => 'Int',
+    'predicate' => 'has_port',
+);
+
 sub run {
     my $self = shift;
 
     Pakket::Server::App->setup( { 'data_dir' => $self->data_dir } );
-    return Pakket::Server::App->to_app;
+    my $app    = Pakket::Server::App->to_app;
+    my $runner = Plack::Runner->new();
+
+    my @runner_opts = (
+        $self->has_port ? ( '--port', $self->port ) : (),
+    );
+
+    $runner->parse_options(@runner_opts);
+    return $runner->run($app);
 }
 
 __PACKAGE__->meta->make_immutable;
