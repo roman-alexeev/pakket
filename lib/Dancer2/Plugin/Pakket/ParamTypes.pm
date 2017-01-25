68db3cccee775beca017a0e2aed7514c4ac35c3e
Sawyer X <xsawyerx@cpan.org>
2017-01-25 15:15:11 +0100

    Rewrite Pakket::Server::App to use repos:
    Instead of hard-coding to a specific backend, we now use
    Repository objects. We're changed the paths to now match the
    method names, and we support type checking, as well as native HTTP
    uploads.

---
+++ lib/Dancer2/Plugin/Pakket/ParamTypes.pm
@@ -0,0 +1,42 @@
+package Dancer2::Plugin::Pakket::ParamTypes;
+
+use strict;
+use warnings;
+use constant { 'HTTP_USER_ERROR' => 400 };
+use Dancer2::Plugin;
+
+extends('Dancer2::Plugin::ParamTypes');
+plugin_keywords('with_types');
+
+sub with_types {
+    my $self = shift;
+    return $self->SUPER::with_types(@_);
+}
+
+sub BUILD {
+    my $self = shift;
+
+    $self->register_type_check(
+		'Str' => sub { defined $_[0] && length $_[0] },
+    );
+
+    $self->register_type_action(
+        'MissingID' => sub {
+            send_error( 'Missing or incorrect ID', HTTP_USER_ERROR() );
+        },
+    );
+
+    $self->register_type_action(
+        'MissingContent' => sub {
+            send_error( 'Missing or incorrect content', HTTP_USER_ERROR() );
+        },
+    );
+
+    $self->register_type_action(
+        'MissingFilename' => sub {
+            send_error( 'Missing or incorrect filename', HTTP_USER_ERROR() );
+        },
+    );
+}
+
+1;
