roman-alexeev/pakket

commit 6525774e884b5d3a84e3efeefed5fbb8453a2dce

Untangle the Scaffolder from the TOML format and Config:
The Scaffolder was the only user of the ConfigReader and
ConfigReader::TOML. It has now moved to Config::Any, which I think
we will end up using all over, not just for the Scaffolder.

Moving this from the Scaffolder namespace will probably be a future
step in having a general configruation for everything.

lib/Pakket/Scaffolder/Role/Config.pm
@@ -2,29 +2,29 @@ package Pakket::Scaffolder::Role::Config;
 # ABSTRACT: scaffolder: role for config
 
 use Moose::Role;
-
 use File::HomeDir;
+use Config::Any;
 use Path::Tiny qw< path >;
 
-use Pakket::ConfigReader;
-
-has pakket_config => (
-    is      => 'ro',
-    isa     => 'HashRef',
-    lazy    => 1,
-    builder => '_build_pakket_config',
+has 'pakket_config' => (
+    'is'      => 'ro',
+    'isa'     => 'HashRef',
+    'lazy'    => 1,
+    'builder' => '_build_pakket_config',
 );
 
 sub _build_pakket_config {
     my $config_file = path( File::HomeDir->my_home, '.pakket' );
-    return +{} unless -f $config_file;
-    my $config_reader = Pakket::ConfigReader->new(
-        'type' => 'TOML',
-        'args' => [ filename => $config_file ],
-    );
-    return $config_reader->read_config;
+
+    my $cfg = Config::Any->load_files({
+        'files'   => [ map "$config_file.$_", qw<json yaml yml conf cfg> ],
+        'use_ext' => 1,
+    });
+
+    return $cfg;
 }
 
 no Moose::Role;
 1;
+
 __END__

lib/Pakket/ConfigReader.pm
@@ -1,45 +1,0 @@
-package Pakket::ConfigReader;
-# ABSTRACT: The Pakket config reader
-
-use Moose;
-use MooseX::StrictConstructor;
-use Module::Runtime qw< use_module >;
-
-has 'type' => (
-	'is'       => 'ro',
-	'isa'      => 'Str',
-	'required' => 1,
-);
-
-has 'args' => (
-	'is'      => 'ro',
-	'isa'     => 'ArrayRef',
-	'default' => sub { +[] },
-);
-
-has 'config_object' => (
-	'is'      => 'ro',
-    'does'    => 'Pakket::Role::ConfigReader',
-	'lazy'    => 1,
-	'builder' => '_build_config_object',
-);
-
-sub _build_config_object {
-	my $self    = shift;
-	my $type    = $self->type;
-	my $package = "Pakket::ConfigReader::$type";
-
-	return use_module($package)->new( @{ $self->args } );
-}
-
-sub read_config {
-	my ( $self, @args ) = @_;
-	my $config = $self->config_object->read_config(@args);
-	return $config;
-}
-
-__PACKAGE__->meta->make_immutable;
-
-no Moose;
-
-1;

lib/Pakket/ConfigReader/TOML.pm
@@ -1,42 +1,0 @@
-package Pakket::ConfigReader::TOML;
-# ABSTRACT: A TOML config reader
-
-use Moose;
-use MooseX::StrictConstructor;
-use TOML::Parser;
-use Types::Path::Tiny qw< Path  >;
-use Carp              qw< croak >;
-
-with qw< Pakket::Role::ConfigReader >;
-
-has 'filename' => (
-    'is'       => 'ro',
-    'isa'      => Path,
-    'coerce'   => 1,
-    'required' => 1,
-);
-
-sub read_config {
-    my $self        = shift;
-    my $config_file = $self->filename;
-    -r $config_file
-        or croak("Config file '$config_file' does not exist or unreadable");
-
-    my $config;
-    eval {
-        $config = TOML::Parser->new( 'strict_mode' => 1 )
-                              ->parse_file($config_file);
-        1;
-    } or do {
-        my $err = $@ || 'Unknown error';
-        croak("Cannot read $config_file: $err");
-    };
-
-    return $config;
-}
-
-__PACKAGE__->meta->make_immutable;
-
-no Moose;
-
-1;

lib/Pakket/Scaffolder/Perl.pm
@@ -9,7 +9,6 @@ use CPAN::Meta::Prereqs;
 use JSON::MaybeXS     qw< decode_json encode_json >;
 use Ref::Util         qw< is_arrayref is_hashref >;
 use Path::Tiny        qw< path    >;
-use TOML              qw< to_toml >;
 use Log::Any          qw< $log    >;
 use Carp ();
 
@@ -174,7 +173,7 @@ sub create_config_for {
     my $conf_path = path( ( $self->config_dir // '.' ), 'perl', $dist_name );
     $conf_path->mkpath;
 
-    my $conf_file = path( $conf_path, "$rel_version.toml" );
+    my $conf_file = path( $conf_path, "$rel_version.json" );
 
     # download source if dir provided and file doesn't already exist
     if ( $self->source_dir ) {
@@ -237,7 +236,7 @@ sub create_config_for {
 
     $self->set_depth( $self->depth - 1 );
 
-    $conf_file->spew_utf8( to_toml($package) );
+    $conf_file->spew_utf8( encode_json($package) );
 }
 
 sub extract_archive {

commit 798137c01e72ac284d521e254c68e2eb3893d740

fix bug: pakket_config has to be a hashref
this fixes a bug introduced by changes in commit 6525774e88
the returned value has to be a HashRef.

lib/Pakket/Scaffolder/Role/Config.pm
@@ -21,7 +21,7 @@ sub _build_pakket_config {
         'use_ext' => 1,
     });
 
-    return $cfg;
+    return +{ @{ $cfg } };
 }
 
 no Moose::Role;