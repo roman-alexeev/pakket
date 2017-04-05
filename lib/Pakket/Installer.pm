e654780f987d0aa6261edf438acc590af17cb524
Dmitry Ulanov <dmitry.ulanov@booking.com>
2017-04-05 14:59:35 +0200

    Split install_package() to make more readable main workflow
    Just split, without changing code and functionality.

--- lib/Pakket/Installer.pm
+++ lib/Pakket/Installer.pm
@@ -92,21 +92,98 @@ sub install_package {
     my ( $self, $package, $dir, $opts ) = @_;
     my $installer_cache = $opts->{'cache'};
 
-    # Are we in a regular (non-bootstrap) mode?
-    # Are we using a bootstrap version of a package?
-    if ( ! $opts->{'skip_prereqs'} && $package->is_bootstrap ) {
-        croak( $log->critical(
-            'You are trying to install a bootstrap version of %s.'
-          . ' Please rebuild this package from scratch.',
-            $package->full_name,
-        ) );
-    }
-
-    # Extracted to use more easily in the install cache below
-    my $pkg_cat  = $package->category;
-    my $pkg_name = $package->name;
+    pre_install_checks($dir, $package, $opts);
 
     $log->debugf( "About to install %s (into $dir)", $package->full_name );
+
+    is_installed($installer_cache, $package)
+        and return;
+
+    mark_as_installed($installer_cache, $package);
+
+    my $parcel_dir
+        = $self->parcel_repo->retrieve_package_parcel($package);
+
+    my $full_parcel_dir = $parcel_dir->child( PARCEL_FILES_DIR() );
+
+    # Get the spec and create a new Package object
+    # This one will have the dependencies as well
+    my $spec_file    = $full_parcel_dir->child( PARCEL_METADATA_FILE() );
+    my $spec         = decode_json $spec_file->slurp_utf8;
+    my $full_package = Pakket::Package->new_from_spec($spec);
+
+    my $prereqs = $full_package->prereqs;
+    foreach my $prereq_category ( keys %{$prereqs} ) {
+        my $runtime_prereqs = $prereqs->{$prereq_category}{'runtime'};
+
+        foreach my $prereq_name ( keys %{$runtime_prereqs} ) {
+            my $prereq_data = $runtime_prereqs->{$prereq_name};
+
+            $self->install_prereq(
+                $prereq_category,
+                $prereq_name,
+                $prereq_data,
+                $dir,
+                $opts
+            );
+        }
+    }
+
+    copy_package_to_install_dir($full_parcel_dir, $dir);
+
+    $self->add_package_in_info_file( $parcel_dir, $dir, $full_package, $opts );
+
+    log_success( sprintf 'Delivering parcel %s', $full_package->full_name );
+
+    return;
+}
+
+sub install_prereq {
+   my ($self, $category, $name, $prereq_data, $dir, $opts) = @_;
+
+   # FIXME: This should be removed when we introduce version ranges
+   # This forces us to install the latest version we have of
+   # something, instead of finding the latest, based on the
+   # version range, which "$prereq_version" contains. -- SX
+   my $ver_rel = $self->parcel_repo->latest_version_release(
+       $category,
+       $name,
+       $prereq_data->{'version'},
+   );
+
+   my ( $version, $release ) = @{$ver_rel};
+
+   my $prereq = Pakket::Requirement->new(
+       'category' => $category,
+       'name'     => $name,
+       'version'  => $version,
+       'release'  => $release,
+   );
+
+   $self->install_package(
+       $prereq, $dir,
+       { %{$opts}, 'as_prereq' => 1 },
+   );
+}
+
+sub copy_package_to_install_dir {
+    my ($full_parcel_dir, $dir) = @_;
+    foreach my $item ( $full_parcel_dir->children ) {
+        my $basename = $item->basename;
+
+        $basename eq PARCEL_METADATA_FILE()
+            and next;
+
+        my $target_dir = $dir->child($basename);
+        dircopy( $item, $target_dir );
+    }
+}
+
+sub is_installed {
+    my ($installer_cache, $package) = @_;
+
+    my $pkg_cat  = $package->category;
+    my $pkg_name = $package->name;
 
     if ( defined $installer_cache->{$pkg_cat}{$pkg_name} ) {
         my $ver_rel = $installer_cache->{$pkg_cat}{$pkg_name};
@@ -135,11 +212,34 @@ sub install_package {
 
         $log->debugf( '%s already installed.', $package->full_name );
 
-        return;
-    } else {
-        $installer_cache->{$pkg_cat}{$pkg_name} = [
-            $package->version, $package->release,
-        ];
+        return 1;
+    }
+
+    return 0;
+}
+
+sub mark_as_installed {
+    my ($installer_cache, $package) = @_;
+
+    my $pkg_cat  = $package->category;
+    my $pkg_name = $package->name;
+
+    $installer_cache->{$pkg_cat}{$pkg_name} = [
+        $package->version, $package->release,
+    ];
+}
+
+sub pre_install_checks {
+    my ($dir, $package, $opts) = @_;
+
+    # Are we in a regular (non-bootstrap) mode?
+    # Are we using a bootstrap version of a package?
+    if ( ! $opts->{'skip_prereqs'} && $package->is_bootstrap ) {
+        croak( $log->critical(
+            'You are trying to install a bootstrap version of %s.'
+          . ' Please rebuild this package from scratch.',
+            $package->full_name,
+        ) );
     }
 
     if ( !is_writeable($dir) ) {
@@ -147,66 +247,6 @@ sub install_package {
             "Can't write to your installation directory ($dir)",
         ) );
     }
-
-    my $parcel_dir
-        = $self->parcel_repo->retrieve_package_parcel($package);
-
-    my $full_parcel_dir = $parcel_dir->child( PARCEL_FILES_DIR() );
-
-    # Get the spec and create a new Package object
-    # This one will have the dependencies as well
-    my $spec_file    = $full_parcel_dir->child( PARCEL_METADATA_FILE() );
-    my $spec         = decode_json $spec_file->slurp_utf8;
-    my $full_package = Pakket::Package->new_from_spec($spec);
-
-    my $prereqs = $full_package->prereqs;
-    foreach my $prereq_category ( keys %{$prereqs} ) {
-        my $runtime_prereqs = $prereqs->{$prereq_category}{'runtime'};
-
-        foreach my $prereq_name ( keys %{$runtime_prereqs} ) {
-            my $prereq_data = $runtime_prereqs->{$prereq_name};
-
-            # FIXME: This should be removed when we introduce version ranges
-            # This forces us to install the latest version we have of
-            # something, instead of finding the latest, based on the
-            # version range, which "$prereq_version" contains. -- SX
-            my $ver_rel = $self->parcel_repo->latest_version_release(
-                $prereq_category,
-                $prereq_name,
-                $prereq_data->{'version'},
-            );
-
-            my ( $prereq_version, $prereq_release ) = @{$ver_rel};
-
-            my $prereq = Pakket::Requirement->new(
-                'category' => $prereq_category,
-                'name'     => $prereq_name,
-                'version'  => $prereq_version,
-                'release'  => $prereq_release,
-            );
-
-            $self->install_package(
-                $prereq, $dir,
-                { %{$opts}, 'as_prereq' => 1 },
-            );
-        }
-    }
-
-    foreach my $item ( $full_parcel_dir->children ) {
-        my $basename = $item->basename;
-
-        $basename eq PARCEL_METADATA_FILE()
-            and next;
-
-        my $target_dir = $dir->child($basename);
-        dircopy( $item, $target_dir );
-    }
-
-    $self->add_package_in_info_file( $parcel_dir, $dir, $full_package, $opts );
-
-    log_success( sprintf 'Delivering parcel %s', $full_package->full_name );
-
-    return;
 }
 
 __PACKAGE__->meta->make_immutable;
