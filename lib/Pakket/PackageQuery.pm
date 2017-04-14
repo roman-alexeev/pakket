6db4af222d7a83a16fd1a69f078e524157a1e6b3
Sawyer X <xsawyerx@cpan.org>
2017-04-14 14:07:29 +0200

    Creating PackageQuery:
    During discussion, we realized that we need to represent three
    types of package-related objects, but only use two classes.
    
    We currently have Pakket::Package and Pakket::Requirement. The
    Package class represents an existing package with its configuration,
    prereqs, and so on. Requirement class represents a specific package
    we would like to use as a requirement, but without all the package
    information.
    
    What we need instead is:
    
    * An object representing the package, with all the details.
      (Pakket::Package)
    * An object representing the requirement, but not in a specific
      version. Instead, it uses a range. (This *should* be
      Pakket::Requirement.)
    * An object in between, that represents what we need as a package
      but without the details. This is what Pakket::Requirement
      currently gives us. We need this in order to do queries, so the
      name Pakket::PackageQuery was picked.
    
    This introduces this class and uses it here and then. I will then
    move to clean up the Requirement class and usages to contain
    ranges and remove the need to create a Requirement when we in fact
    want a PackageQuery object.

---
+++ lib/Pakket/PackageQuery.pm
@@ -0,0 +1,55 @@
+package Pakket::PackageQuery;
+# ABSTRACT: An object representing a query for a package
+
+use Moose;
+use MooseX::StrictConstructor;
+
+use Carp              qw< croak >;
+use Log::Any          qw< $log >;
+use Pakket::Constants qw<
+    PAKKET_PACKAGE_SPEC
+    PAKKET_DEFAULT_RELEASE
+>;
+use Pakket::Types;
+
+with qw< Pakket::Role::BasicPackageAttrs >;
+
+has [qw< name category version >] => (
+    'is'       => 'ro',
+    'isa'      => 'Str',
+    'required' => 1,
+);
+
+has 'release' => (
+    'is'      => 'ro',
+    'isa'     => 'PakketRelease',
+    'coerce'  => 1,
+    'default' => sub { PAKKET_DEFAULT_RELEASE() },
+);
+
+has 'is_bootstrap' => (
+    'is'      => 'ro',
+    'isa'     => 'Bool',
+    'default' => sub {0},
+);
+
+sub new_from_string {
+    my ( $class, $req_str ) = @_;
+
+    if ( $req_str !~ PAKKET_PACKAGE_SPEC() ) {
+        croak( $log->critical("Cannot parse $req_str") );
+    } else {
+        # This shuts up Perl::Critic
+        return $class->new(
+            'category' => $1,
+            'name'     => $2,
+            'version'  => $3,
+            ( 'release'  => $4 )x!! $4,
+        );
+    }
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
